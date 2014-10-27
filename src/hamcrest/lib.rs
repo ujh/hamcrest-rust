210bd09ff7bd29b56699ba7f74da36767e34a697
Oliver Mader <b52@reaktor42.de>
2014-10-27 20:43:25 +0100

    Add `close_to` matcher for floating numbers

--- src/hamcrest/lib.rs
+++ src/hamcrest/lib.rs
@@ -8,12 +8,14 @@ pub use matchers::is::{is, is_not};
 pub use matchers::is::is_not as not;
 pub use matchers::none::{none};
 pub use matchers::equal_to::equal_to;
+pub use matchers::close_to::{close_to, close_to_eps};
 pub use matchers::existing_path::{existing_path, existing_file, existing_dir};
 pub use matchers::vecs::{of_len, contains};
 
 pub mod core;
 pub mod matchers {
   pub mod equal_to;
+  pub mod close_to;
   pub mod existing_path;
   pub mod is;
   pub mod none;
