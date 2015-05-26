59ceb2f31c79fba5d63db42e74aaaab13f3596c6
Oliver Mader <b52@reaktor42.de>
2015-05-26 15:42:58 +0200

    Proper floating point comparisons for `CloseTo`

--- src/matchers/close_to.rs
+++ src/matchers/close_to.rs
@@ -1,66 +1,86 @@
-use num::{Float};
-use num::traits::cast;
-use std::fmt::{self, Formatter};
+use num::{Float, Zero};
+use std::fmt::{self, Display, Debug, Formatter};
+use std::{f32, f64};
 use {success, Matcher, MatchResult};
 
+/// Compares two floating point values for equality.
+///
+/// The comparison is based on a relative error metric and uses special
+/// fallbacks for certain edge cases like very small numbers. The exact
+/// algorithm is described [here](http://floating-point-gui.de/errors/comparison/).
 pub struct CloseTo<T> {
     expected: T,
-    delta: T,
+    epsilon: T,
 }
 
-impl<T: fmt::Debug> fmt::Display for CloseTo<T> {
+impl<T: Debug> Display for CloseTo<T> {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         self.expected.fmt(f)
     }
 }
 
-impl<T: Float + fmt::Debug> Matcher<T> for CloseTo<T> {
-    fn matches(&self, actual: T) -> MatchResult {
-        // Handle cases like infinity / nan
-        if self.expected == actual {
-            return success();
-        }
+/// This is just a fix until rust-lang/num#93 is fixed.
+pub trait FloatMinPositive {
+    /// Returns the smallest positive, normalized value that this type can represent.
+    fn min_positive_value() -> Self;
+}
 
-        let delta = (self.expected - actual).abs() - self.delta;
-
-        if delta <= cast(0.0).unwrap() {
-            return success();
-        }
-
-        Err(format!("was {:?}", actual))
+impl FloatMinPositive for f32 {
+    fn min_positive_value() -> Self {
+        f32::MIN_POSITIVE
     }
 }
 
-pub fn close_to<T>(expected: T, delta: T) -> CloseTo<T> {
+impl FloatMinPositive for f64 {
+    fn min_positive_value() -> Self {
+        f64::MIN_POSITIVE
+    }
+}
+
+impl<T: Float + Zero + FloatMinPositive + Debug> Matcher<T> for CloseTo<T> {
+    fn matches(&self, actual: T) -> MatchResult {
+        let a = self.expected.abs();
+        let b = actual.abs();
+
+        let d = (a - b).abs();
+
+        let close =
+            // shortcut, handles infinities
+            a == b
+            // a or b is zero or both are extremely close to it
+            // relative error is less meaningful here
+            || ((a == Zero::zero() || b == Zero::zero() || d < FloatMinPositive::min_positive_value()) &&
+                d < (self.epsilon * FloatMinPositive::min_positive_value()))
+            // use relative error
+            || d / (a + b).min(Float::max_value()) < self.epsilon;
+
+        if close {
+            success()
+        } else {
+            Err(format!("was {:?}", actual))
+        }
+    }
+}
+
+pub fn close_to<T>(expected: T, epsilon: T) -> CloseTo<T> {
     CloseTo {
         expected: expected,
-        delta: delta
+        epsilon: epsilon
     }
 }
 
 #[cfg(test)]
 mod test {
     use std::f64;
-    use std::thread;
-    use {assert_that,is,close_to};
+    use {assert_that,is,not,close_to};
 
     #[test]
     fn test_equality_of_floats() {
-        // Successful match
         assert_that(1.0f64, is(close_to(1.0, 0.00001)));
         assert_that(f64::INFINITY, is(close_to(f64::INFINITY, 0.00001)));
-        assert_that(1e-40f64, is(close_to(0.0, 0.01)));
-        assert_that(1e-40f64, is(close_to(0.0, 0.000001)));
         assert_that(1e-40f32, is(close_to(0.0, 0.01)));
-        assert_that(1e-40f32, is(close_to(0.0, 0.000001)));
-
-        // Unsuccessful match
-        assert!(thread::spawn(|| {
-            assert_that(2.0, is(close_to(1.0f64, 0.0001)));
-        }).join().is_err());
-
-        assert!(thread::spawn(move || {
-            assert_that(f64::NAN, is(close_to(f64::NAN, 0.0001)));
-        }).join().is_err());
+        assert_that(1e-40f32, is(not(close_to(0.0, 0.000001))));
+        assert_that(2.0, is(not(close_to(1.0f64, 0.00001))));
+        assert_that(f64::NAN, is(not(close_to(f64::NAN, 0.00001))));
     }
 }
