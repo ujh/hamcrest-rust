77103b45dea7dd905f0919c93087c395badf9d0f
Urban Hafner <contact@urbanhafner.com>
2016-10-05 17:14:04 +0200

    Make none tests integration tests

---
+++ tests/none.rs
@@ -0,0 +1,26 @@
+// Copyright 2016 Urban Hafner
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#[macro_use] extern crate hamcrest;
+
+mod none {
+
+    use hamcrest::prelude::*;
+
+    #[test]
+    fn none_is_none() {
+        assert_that!(None, is(none::<i8>()));
+    }
+
+    #[test]
+    fn some_is_not_none() {
+        let matcher = is_not(none());
+        assert_that!(Some(1), matcher);
+    }
+
+}
