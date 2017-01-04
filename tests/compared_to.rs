034f3119588286c20f709c698f7bb0362eac1d31
Matthew Alexander LaChance <matt@istheguy.com>
2017-01-04 08:14:03 -0500

    Be sure less_than and greater_than disallow equality

--- tests/compared_to.rs
+++ tests/compared_to.rs
@@ -24,6 +24,12 @@ mod compared_to {
     }
 
     #[test]
+    #[should_panic]
+    fn less_than_is_not_equal() {
+        assert_that!(2, is(less_than(2)));
+    }
+
+    #[test]
     fn ints_greater_than() {
         assert_that!(8, is(greater_than(5)));
     }
@@ -32,6 +38,12 @@ mod compared_to {
     #[should_panic]
     fn unsuccessful_greater_than() {
         assert_that!(1, is(greater_than(3)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_is_not_equal() {
+        assert_that!(2, is(greater_than(2)));
     }
 
 }
